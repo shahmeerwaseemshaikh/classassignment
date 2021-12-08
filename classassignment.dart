a8aff4added4a8f72ba8bd15cdaa35c6b7ddd047
Shahmeer Shaikh <65039014+shahmeerwaseemshaikh@users.noreply.github.com>
2021-12-07 23:23:39 -0800

    Add files via upload

---
+++ classassignment.dart
@@ -0,0 +1,119 @@
+import 'dart:io';
+
+import 'dart:math';
+
+void main() {
+  // stdout.write("Enter Your Fav Mobile : ");
+  // String a= stdin.readLineSync()!;
+  // print(a.length);
+
+// String name="Pakistani";
+// print(name.indexOf("n"));
+
+// stdout.write("Enter Your Name : ");
+// String name=stdin.readLineSync()!;
+// stdout.write("Enter Your Roll Number : ");
+// int Roll=int.parse(stdin.readLineSync()!);
+// int total= 300;
+
+// stdout.write("Enter Subject 1 Mask : ");
+// int a=int.parse(stdin.readLineSync()!);
+// if (a>=100) {
+//  stdout.write("Enter Subject 2 Mask");
+//   int b=int.parse(stdin.readLineSync()!);
+
+// if (b>=100) {
+//  stdout.write("Enter Subject  Mask");
+//   int c=int.parse(stdin.readLineSync()!);
+//  }
+ 
+// else{
+  
+// }
+
+// stdout.write("Enter Subject 2 Mask : ");
+
+
+
+// stdout.write("Enter Subject 3 Mask : ");
+// int c=int.parse(stdin.readLineSync()!);
+// if (c>=100) {
+//   print("You Enter Incorrect Number");
+// }
+
+
+
+
+//  stdout.write("Enter the name ");
+// String Username = stdin.readLineSync()!;
+// stdout.write("Enter the Roll Number ");
+// int? Roll = int.parse(stdin.readLineSync()!);
+
+// int Total = 300;
+
+// stdout.write("Enter the Marks of First Subject :  ");
+// int? A = int.parse(stdin.readLineSync()!);
+// if(A>=100){
+//   print("Enter the Correct Marks");
+  
+// }
+// else{
+// stdout.write("Enter the Marks of Second Subject  ");
+// int? b = int.parse(stdin.readLineSync()!);
+
+// if(b>=100){
+//    print("you Enter the Correct Marks ");
+// }
+
+// else{
+//   stdout.write("Enter the Marks of Third Subject ");
+//  int? c = int.parse(stdin.readLineSync()!);
+// if(c>=100){
+//    print("you Enter the Correct Marks ");
+
+// }
+//  int Obt = A+b+c;
+//  print("${Obt} out of 300");
+
+//  double Per = Obt/Total*100;
+//  print("Your Percentage${Per}%");
+
+// String Grade1 , Grade2 , Grade3 , Grade4;
+// if (Per>=90){
+//   Grade1 ="Grade A+";
+//   print("${Grade1}");
+// } 
+
+// else if (Per>=80){
+//   Grade2 = "Grade A";
+//   print(Grade2);
+// }
+
+// else if(Per>=70){
+//   Grade3 = "Grade B";
+// print(Grade3);
+// }
+
+// else if (Per>=60){
+// Grade4 = "Grade C";
+// print(Grade4);
+// }
+
+// else{
+//   print("You are Fail");
+// }
+// }
+// }
+
+
+
+// int v1=Random().nextInt(3);
+// int a=1;
+// if (v1==a) {
+//   print("Bingo! Corrrect Answer");
+// }
+// else{
+//   print("Incorrect");
+// }
+
+}
